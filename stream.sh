482d7483301242eb4fca74a1abfccf6f16e95875
Robert Hu <robert.hu@me.com>
2023-01-09 18:52:03 +0800

    modify ffmpeg scale

--- stream.sh
+++ stream.sh
@@ -7,15 +7,15 @@ FFSERVER_LOG_LEVEL="error"
 FFMPEG_LOG_LEVEL="warning"
 FFMPEG_INPUT_OPTS="-use_wallclock_as_timestamps 1"
 FFMPEG_OUTPUT_OPTS="-async 1 -vsync 1"
-FFMPEG_SCALE="384:216"
+FFMPEG_SCALE="iw*.25:ih*.25"
 
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-echo "url:" ${RTSP_URL}
+echo "url:" ${F1_URL}
 
 ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
-echo "rtsp:" ${RTSP_URL}
+echo "rtsp:" ${F2_URL}
 ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${F1_URL} -vf scale=${FFMPEG_SCALE} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/f1.ffm
 
 ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${F2_URL} -vf scale=${FFMPEG_SCALE} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/f2.ffm
