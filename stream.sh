9415c7ea5a06526ba6c8f46d06cc9495e8756b2c
Robert Hu <robert.hu@me.com>
2023-01-09 20:12:26 +0800

    modify stream.sh

--- stream.sh
+++ stream.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-F1_URL="rtsp://172.16.1.30/gama.avi"
-F2_URL="rtsp://172.16.1.30/cut.avi"
+F1_URL="rtsp://172.16.1.30/cut.avi"
+F2_URL="rtsp://172.16.1.30/gama.avi"
 
 FFSERVER_LOG_LEVEL="error"
 FFMPEG_LOG_LEVEL="warning"
@@ -9,13 +9,10 @@ FFMPEG_INPUT_OPTS="-use_wallclock_as_timestamps 1"
 FFMPEG_OUTPUT_OPTS="-async 1 -vsync 1"
 FFMPEG_SCALE="iw*.25:ih*.25"
 
-trap "exit" INT TERM ERR
-trap "kill 0" EXIT
-
-echo "url:" ${F1_URL}
-
+#trap "exit" INT TERM ERR
+#trap "kill 0" EXIT
+echo "stream 1:" ${F1_URL}
 ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
-echo "rtsp:" ${F2_URL}
-ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${F1_URL} -vf scale=${FFMPEG_SCALE} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/f1.ffm
-
-ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${F2_URL} -vf scale=${FFMPEG_SCALE} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/f2.ffm
+ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${F1_URL} -vf scale=${FFMPEG_SCALE} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/f1.ffm &
+ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${F2_URL} -vf scale=${FFMPEG_SCALE} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/f2.ffm 
+echo "stream 1:" ${F1_URL}
